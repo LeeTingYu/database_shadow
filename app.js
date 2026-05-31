2b404ef4ccc4b8997388f9bca5120f56cb32570d
LeeTingYu <leenina831831@gmail.com>
2026-05-31 10:54:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // 色階：深藍（涼爽）→ 深紅（炎熱）
 // ============================================
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://clench-outrank-moustache.ngrok-free.dev';
 
 // ============ 地圖初始化 ============
 const map = L.map('map', {
